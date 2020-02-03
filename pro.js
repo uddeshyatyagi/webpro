a779a2ea16372d6f748ee2332d24df3d861616f6
Uddeshya Tyagi <54842397+uddeshyatyagi@users.noreply.github.com>
2020-02-04 01:16:07 +0530

    Add files via upload

---
+++ pro.js
@@ -0,0 +1,9 @@
+
+cons password=document.querySelector(".password")
+const password="ucantgetit";
+if(password==="ucantgetit")
+{
+console.log("Log In Successful");
+}
+else 
+console.log("Enter Valid Password");
